e222eb1682a2e0bf1df90350af85a43e7e0c3860
James-Crockett <nitheesh.gnk777@gmail.com>
2026-06-16 18:02:39 -0700

    core: added payment request model'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
 
 app = FastAPI()
+
+# defining datamodel for request
+# using pydantic to handle validation
+class PaymentRequest(BaseModel):
+    amount: float = Field(gt=0)                 # amount should always be +ve and greater than 0.
+    description: str = Field(min_length=1)      # cannot be an empty string
 
 @app.get("/")
 def root():
     return {
-        "message": "Payment backend"
+        "message": "Payment backend running"
     }
 
+@app.post("/api/payments")
+def create_payment(payment: PaymentRequest):
+    return{
+        "success": True,
+        "status":"SUCCESS",
+        "amount": payment.amount,
+        "description": payment.description,
+        "message": "Payment endpoint working"
+    }
