ffae1ba4ac498b60446715a59305602f60bedbf3
James-Crockett <nitheesh.gnk777@gmail.com>
2026-06-17 12:06:33 -0700

    core(clover): added clover_service

---
+++ app/clover_service.py
@@ -0,0 +1,55 @@
+import os
+import request
+from dotenv import load_dotenv
+
+# for loading env file
+load_dotenv()
+
+# loading env variables
+CLOVER_BASE_URL= os.getenv("CLOVER_BASE_URL")
+CLOVER_MERCHANT_ID= os.getenv("CLOVER_MERCHANT_ID")
+CLOVER_ACCESS_TOKEN= os.getenv("CLOVER_ACCESS_TOLKEN")
+
+# auth call and data format
+def get_headers():
+    return{
+        "Authorization": f"Bearer {CLOVER_ACCESS_TOKEN}",
+        "Content-Type": "application/json",
+    }
+
+# creating order id
+def create_order():
+    # merchant order dir
+    url = f"{CLOVER_BASE_URL}/v3/merchants/{CLOVER_MERCHANT_ID}/orders"
+
+    # post, in url location, auth call, empty body for blank order
+    response = requests.post(
+        url,
+        headers = get_headers(),
+        json = {}
+    )
+
+    # raise exception if any error
+    response.raise_for_status()
+    return response.json()
+
+# adding items
+def add_line_item (order_id: str, description: str, amount_cents: int):
+    #order id dir
+    url = f"{CLOVER_BASE_URL}/v3/merchants/{CLOVER_MERCHANT_ID}/orders{order_id}/line_items"
+
+    # item
+    payload= {
+        "name": description,
+        "price": amount_cents
+    }
+
+    # post, in url location, auth call, order info
+    response = requests.post(
+        url,
+        headers = get_headers(),
+        json = payload
+    )
+    # raise exception if any error
+    response.raise_for_status()
+    return response.json()
